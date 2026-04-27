e201cdead0af68834672b499fc2191eb5ce74b24
onmidnightblue <onmidnightblue@github.com>
2026-04-27 10:20:18 +0900

    [Fixed] edit mode loading spinner

--- src/components/admin/RestaurantListItem.tsx
+++ src/components/admin/RestaurantListItem.tsx
@@ -125,12 +125,18 @@ const RestaurantListItem = ({ restaurant }: Props) => {
         } select-none`}
         onClick={() => setIsEditMode((prev) => !prev)}
       >
-        {isLoading && (
-          <div className="absolute inset-y-0 right-0 flex items-center pr-1 pointer-events-none">
-            <div className="w-3 h-3 border-2 border-blue-500 rounded-full border-t-transparent animate-spin"></div>
-          </div>
+        {isEditMode ? (
+          <>
+            {isLoading && (
+              <div className="absolute inset-y-0 right-0 flex items-center pr-1 pointer-events-none">
+                <div className="w-3 h-3 border-2 border-blue-500 rounded-full border-t-transparent animate-spin" />
+              </div>
+            )}
+            {!isLoading && "VIEW"}
+          </>
+        ) : (
+          "EDIT"
         )}
-        {!isLoading && isEditMode ? "VIEW" : "EDIT"}
       </div>
     </li>
   );
